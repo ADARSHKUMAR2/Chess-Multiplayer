2a605a2129d31833da6d31e1487d1dd952308560
Adarsh <>
2022-08-06 17:17:46 +0530

    Added board initial setup

--- Assets/Scripts/Board/Board.cs
+++ Assets/Scripts/Board/Board.cs
@@ -1,18 +1,16 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(SquareSelectorCreator))]
 public class Board : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] private Transform bottomLeftSquareTransform;
+    [SerializeField] private float squareSize;
 
-    // Update is called once per frame
-    void Update()
+    public Vector3 CalculatePositionFromCoords(Vector2Int coords)
     {
-        
+        return bottomLeftSquareTransform.position + new Vector3(coords.x * squareSize, 0f, coords.y * squareSize);
     }
 }
